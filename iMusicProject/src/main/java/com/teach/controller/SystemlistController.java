d75530607425ab407f4fd7a4918047639a637022
忆笑容依旧 <@LAPTOP-VHP3SN34>
2019-08-31 16:54:24 +0800

    提交为提交完的

--- iMusicProject/src/main/java/com/teach/controller/SystemlistController.java
+++ iMusicProject/src/main/java/com/teach/controller/SystemlistController.java
@@ -1,5 +1,15 @@
 package com.teach.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.teach.mapper.SystemlistMapper;
+
+@Controller
 public class SystemlistController {
+	@Autowired
+	private SystemlistMapper mapper;
+	
+	
 
 }
